cc4b4d90dd9f4c4903aecb3f6dd4e6ac61bad786
EricHo0129 <yung.ho@104.com.tw>
2018-09-18 16:25:35 +0800

    Job啟動的時候就把舊資料清掉

--- src/main/java/com/eric/springbatch/core/JobCompletionNotificationListener.java
+++ src/main/java/com/eric/springbatch/core/JobCompletionNotificationListener.java
@@ -1,5 +1,10 @@
 package com.eric.springbatch.core;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
@@ -28,15 +33,26 @@ public class JobCompletionNotificationListener extends JobExecutionListenerSuppo
 	public void beforeJob(JobExecution jobExecution) {
 		// 清除舊資料
 		boolean clean = false;
+//		if (clean) {
+//			int remain = 4; //留4筆與將要跑的1筆=5筆
+//			jdbcTemplate.update("delete from batch_step_execution_context");
+//			jdbcTemplate.update("delete from batch_step_execution where JOB_EXECUTION_ID < (select ID from batch_job_seq) - ?", remain);
+//			jdbcTemplate.update("delete from batch_job_execution_params where JOB_EXECUTION_ID < (select ID from batch_job_seq) - ?", remain);
+//			jdbcTemplate.update("delete from batch_job_execution_context where JOB_EXECUTION_ID < (select ID from batch_job_seq) - ?", remain);
+//			jdbcTemplate.update("delete from batch_job_execution where JOB_EXECUTION_ID < (select ID from batch_job_seq) - ?", remain);
+//			int result = jdbcTemplate.update("delete from batch_job_instance where JOB_INSTANCE_ID < (select ID from batch_job_seq) - ?", remain);
+//			log.info("!!! OLD JOB HAS BEEN DELETED! ("+result+")");			
+//		}
 		if (clean) {
-			int remain = 4; //留4筆與將要跑的1筆=5筆
-			jdbcTemplate.update("delete from batch_step_execution_context");
-			jdbcTemplate.update("delete from batch_step_execution where JOB_EXECUTION_ID < (select ID from batch_job_seq) - ?", remain);
-			jdbcTemplate.update("delete from batch_job_execution_params where JOB_EXECUTION_ID < (select ID from batch_job_seq) - ?", remain);
-			jdbcTemplate.update("delete from batch_job_execution_context where JOB_EXECUTION_ID < (select ID from batch_job_seq) - ?", remain);
-			jdbcTemplate.update("delete from batch_job_execution where JOB_EXECUTION_ID < (select ID from batch_job_seq) - ?", remain);
-			int result = jdbcTemplate.update("delete from batch_job_instance where JOB_INSTANCE_ID < (select ID from batch_job_seq) - ?", remain);
-			log.info("!!! OLD JOB HAS BEEN DELETED! ("+result+")");			
+			List<String> sqlList = Stream.of(
+					"delete from batch_step_execution_context where STEP_EXECUTION_ID <= (select STEP_EXECUTION_ID from batch_step_execution where JOB_EXECUTION_ID < (select ID from batch_job_seq) order by JOB_EXECUTION_ID desc limit 1)",
+					"delete from batch_step_execution where JOB_EXECUTION_ID < (select ID from batch_job_seq)",
+					"delete from batch_job_execution_params where JOB_EXECUTION_ID < (select ID from batch_job_seq)",
+					"delete from batch_job_execution_context where JOB_EXECUTION_ID < (select ID from batch_job_seq)",
+					"delete from batch_job_execution where JOB_EXECUTION_ID < (select ID from batch_job_seq)",
+					"delete from batch_job_instance where JOB_INSTANCE_ID < (select ID from batch_job_seq)"
+					).collect(Collectors.toList());
+			jdbcTemplate.batchUpdate(sqlList.toArray(new String[0]));
 		}
 	}
 }
